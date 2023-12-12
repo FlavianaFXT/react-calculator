17a15efda64b7d77b8b4f7d89b11277a69038820
Flaviana <flaviferraz@yahoo.com.br>
2023-12-11 22:29:51 -0300

    criação do projeto com yarn e instalação styled-components; primeiras estilizações

---
+++ react-calculator/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Content } from './styles'
+import Button from './components/Button'
+import Input from './components/Input'
+
+const App = () => {
+  return (
+      <Content>
+          <h1>Hello!!!</h1>
+          <Button />
+          <Input />
+      </Content>
+  )
+}
+
+export default App
---
+++ react-calculator/src/global.js
@@ -0,0 +1,9 @@
+import  styled from 'styled-components';
+
+export const GlobalStyleComponent = styled.div`
+    *, body{
+        margin: 0;
+        padding: 0;
+    }
+`
+
